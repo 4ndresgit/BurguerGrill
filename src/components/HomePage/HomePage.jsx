e8da29e3ff9a22b54d6ffb0586461b90984d1192
Andres Rossi <andresrossi2022@gmail.com>
2023-07-06 09:45:15 -0300

    HomePage img

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ProductListContainer from '../ProductListContainer/ProductListContainer';
+import  takeaway  from '../../assets/images/takeaway.jpg'
 
 const HomePage = () => {
   return (
     <div>
+      <img
+        src={takeaway}
+        alt="Take-Away"
+        style={{ width: "100%" }}
+      />
       <ProductListContainer />
     </div>
   );
