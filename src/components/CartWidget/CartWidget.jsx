3ef52a33327f6be633473999131c58f44ca447ae
Andres Rossi <andresrossi2022@gmail.com>
2023-07-06 12:56:09 -0300

    Cartwidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,13 +1,17 @@
-
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import Badge from 'react-bootstrap/Badge';
+import { useCart } from '../../context/CartContext';
 
 function CartWidget() {
+  const { cart } = useCart();
+  const cartItemCount = cart.reduce((total, product) => total + product.quantity, 0);
+
   return (
     <div>
       <FontAwesomeIcon icon={faShoppingCart} size='lg' />
-      <Badge variant='primary'>3</Badge>
+      <Badge variant='primary'>{cartItemCount}</Badge>
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import "./NavBar.scss";
 import logo from "../../assets/images/burguer.svg";
 import CartWidget from "../CartWidget/CartWidget";
-import { useCart } from "../../Context/CartContext";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 const NavBar = ({ userLoggedIn }) => {
@@ -12,12 +11,12 @@ const NavBar = ({ userLoggedIn }) => {
       <div className="navbar-container">
         <img src={logo} alt="logo" />
         <div className="navbar-links">
-        <Link to="/" className="navbar-brand">
-          Take away
-        </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-        <NavDropdown title="Categorías" id="basic-nav-dropdown">
+          <Link to="/" className="navbar-brand">
+            Take away
+          </Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <NavDropdown title="Categorías" id="basic-nav-dropdown">
               <Link to="/category/burgers" className="dropdown-item">
                 Burgers
               </Link>
@@ -31,24 +30,22 @@ const NavBar = ({ userLoggedIn }) => {
                 Sauces
               </Link>
             </NavDropdown>
-          <Nav className="mr-auto">
-            <Link to="/about" className="nav-link">
-              Nosotros
-            </Link>
-            {userLoggedIn ? (
-              <Link to="/cart" className="nav-link">
-                Carrito
+            <Nav className="mr-auto">
+              <Link to="/about" className="nav-link">
+                Nosotros
               </Link>
-            ) : (
-              <Link to="/signin" className="nav-link">
-                Login
-              </Link>
-            )}
-            
-          </Nav>
-        </Navbar.Collapse>
+              
+                <Link to="/cart" className="nav-link">
+                  <CartWidget />
+                </Link>
+              
+                <Link to="/signin" className="nav-link">
+                  Login
+                </Link>
+              
+            </Nav>
+          </Navbar.Collapse>
         </div>
-        
       </div>
     </Navbar>
   );
